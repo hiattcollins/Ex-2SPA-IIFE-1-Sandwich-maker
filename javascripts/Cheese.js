cf6c5c6ac0c7b2509b2f0743f3adfd6be25cad02
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-08-11 11:42:09 -0500

    cleaned up code and styling and added comments

--- javascripts/Cheese.js
+++ javascripts/Cheese.js
@@ -2,24 +2,18 @@ console.log("Cheese.js");
 
 var SandwichMaker = (function(maker) {
 
- 
   var cheesePrices = {"cheddar": 0.50, "swiss": 0.50, "provolone": 0.50, "pepper_jack": 0.50, "american": 0.50};
   
-
   maker.addCheese = function(inputCheese) {
 
     var totalCheesePrice = 0;
-  	console.log("addBread activated");
 
   	inputCheese.forEach(function(element) {
   		totalCheesePrice += cheesePrices[element];
   	});
     
-    console.log("totalCheesePrice", totalCheesePrice);
-
     SandwichMaker.addTopping(totalCheesePrice);
   };
-
 
   return maker;
 })(SandwichMaker || {});
